91464f78867b47d2f1e029c0646223c93416d410
Daniel Grathwol <dg@air-von-daniel.eduroam1.wireless.local>
2021-10-26 17:14:37 +0200

    Second Commit

---
+++ src/MyClient.java
@@ -0,0 +1,28 @@
+package datasecurity_rmi.src;
+import java.rmi.Naming;
+import java.rmi.RMISecurityManager;
+
+
+public class MyClient {
+    public static void main(String argv[]) {
+        // set the RMI Security Manager, in case we need to load remote
+        // classes
+        System.setSecurityManager(new RMISecurityManager());
+
+        String url = "rmi://osprey.unf.edu/";   // server
+        try {
+         // Lookup account object
+        PrinterServer printer1 =
+                (PrinterServer)Naming.lookup(url + "Printer1");        
+     //Make a deposit
+        printer1.print("NewDocument", "Printer1");
+
+     }
+     catch (Exception e) {               
+     System.out.println("Error looking up printer");
+        e.printStackTrace();
+     }
+     System.exit(0);
+   }
+
+}
---
+++ src/PrinterServerImpl.java
@@ -0,0 +1,69 @@
+package datasecurity_rmi.src;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.rmi.RemoteException;
+
+public class PrinterServerImpl extends UnicastRemoteObject implements PrinterServer  {
+    private String printerName = "";
+    private String fileName = "";
+    private ArrayList printerList;
+
+    public PrinterServerImpl(String aName) throws RemoteException {
+        printerName = aName;
+    }
+
+    @Override
+    public void print(String filename, String printer) throws RemoteException {
+        // TODO Auto-generated method stub
+        System.out.println(filename + " " + printer); 
+    }
+
+    @Override
+    public void queue(String printer) throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void topQueue(String printer, int job) throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void start() throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void stop() throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void restart() throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void status(String printer) throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void readConfig(String parameter) throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setConfig(String parameter, String value) throws RemoteException {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
