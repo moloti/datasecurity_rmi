413f546e729fa235c74cf11c42936ebf3d33da9e
Daniel Grathwol <danielgrathwol@gmail.com>
2021-11-18 14:49:57 +0100

    Working Maven Setup

---
+++ src/main/java/server/ApplicationServer.java
@@ -0,0 +1,34 @@
+package server;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+import server.PrinterServiceImpl;
+// import datasecurity_rmi.src.RMISSLClientSocketFactory;
+// import datasecurity_rmi.src.RMISSLServerSocketFactory;
+
+public class ApplicationServer extends UnicastRemoteObject {
+
+    public ApplicationServer() throws Exception {
+        super(Registry.REGISTRY_PORT/* , new RMISSLClientSocketFactory(), new RMISSLServerSocketFactory() */);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            // Create SSL-based registry
+            Registry registry = LocateRegistry.createRegistry(
+                    Registry.REGISTRY_PORT/* , new RMISSLClientSocketFactory(), new RMISSLServerSocketFactory() */);
+
+            PrinterService server = new PrinterServiceImpl();
+
+            // Bind this object instance to the name "HelloServer"
+            registry.rebind("PrinterService", server);
+
+            System.out.println("Server Started");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
