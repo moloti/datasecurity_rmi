8329e168d3f100bbdf5aadfbed72e2b479791c16
thebrard <thomas.hebrard134@gmail.com>
2021-11-19 15:33:18 +0100

    create user

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,46 +1,6 @@
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT NOT NULL primary key,
-    user_name VARCHAR (250) NOT NULL,
-    password VARCHAR (250) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS transactions (
-    transaction_id INT NOT NULL primary key,
-    transaction_name VARCHAR (
-        250
-    ) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS roles (
-    role_id INT NOT NULL primary key,
-    role_name VARCHAR (
-        250
-    ) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS user_role (
-    user_role_id INT NOT NULL primary key,
-    role_id INT NOT NULL,
-    user_id INT NOT NULL,
-    constraint fk_role foreign key(role_id) references roles(role_id),
-    constraint fk_user foreign key(user_id) references users(user_id)
-);
-CREATE TABLE IF NOT EXISTS role_transaction (
-    role_action_id INT NOT NULL primary key,
-    role_id INT NOT NULL,
-    transaction_id INT NOT NULL,
-    constraint fk_role foreign key(role_id) references roles(role_id),
-    constraint fk_transaction foreign key(transaction_id) references transactions(transaction_id)
-);
-CREATE TABLE IF NOT EXISTS acl (
-    acl_id INT NOT NULL primary key,
-    user_id INT NOT NULL,
-    print BOOLEAN NOT NULL,
-    queue BOOLEAN NOT NULL,
-    topQueue BOOLEAN NOT NULL,
-    START BOOLEAN NOT NULL,
-    stop BOOLEAN NOT NULL,
-    restart BOOLEAN NOT NULL,
-    status BOOLEAN NOT NULL,
-    readConfig BOOLEAN NOT NULL,
-    setConfig BOOLEAN NOT NULL,
-    manageEmployee BOOLEAN NOT NULL,
-    constraint fk_user foreign key(user_id) references users(user_id)
-);
+CREATE TABLE IF NOT EXISTS users (user_id VARCHAR(255) DEFAULT gen_random_uuid (), user_name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, primary key (user_id));
+CREATE TABLE IF NOT EXISTS transactions (transaction_id VARCHAR(255) DEFAULT gen_random_uuid (), transaction_name VARCHAR(255) NOT NULL, primary key (transaction_id));
+CREATE TABLE IF NOT EXISTS roles (role_id VARCHAR(255) DEFAULT gen_random_uuid (), role_name VARCHAR(255) NOT NULL, primary key (role_id));
+CREATE TABLE IF NOT EXISTS user_role (user_role_id VARCHAR(255) DEFAULT gen_random_uuid (), role_id VARCHAR(255) NOT NULL, user_id VARCHAR(255) NOT NULL, primary key (user_role_id), constraint fk_role foreign key(role_id) references roles(role_id), constraint fk_user foreign key(user_id) references users(user_id));
+CREATE TABLE IF NOT EXISTS role_transaction (role_transaction_id VARCHAR(255) DEFAULT gen_random_uuid (), role_id VARCHAR(255) NOT NULL, transaction_id VARCHAR(255) NOT NULL, primary key (role_transaction_id), constraint fk_role foreign key(role_id) references roles(role_id), constraint fk_transaction foreign key(transaction_id) references transactions(transaction_id));
+CREATE TABLE IF NOT EXISTS acl (acl_id VARCHAR(255) DEFAULT gen_random_uuid (), user_id VARCHAR(255) NOT NULL, print BOOLEAN NOT NULL, queue BOOLEAN NOT NULL, topQueue BOOLEAN NOT NULL, START BOOLEAN NOT NULL, stop BOOLEAN NOT NULL, restart BOOLEAN NOT NULL, status BOOLEAN NOT NULL, readConfig BOOLEAN NOT NULL, setConfig BOOLEAN NOT NULL, primary key (acl_id), constraint fk_user foreign key(user_id) references users(user_id));
