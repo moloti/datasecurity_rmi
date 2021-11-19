d08ea6213da75887f026c152468623f01f7560d2
thebrard <thomas.hebrard134@gmail.com>
2021-11-19 14:50:22 +0100

    fix database connector

--- src/main/java/database/DatabaseConnector.java
+++ src/main/java/database/DatabaseConnector.java
@@ -5,34 +5,15 @@ import java.sql.*;
 public class DatabaseConnector {
     private Connection conn;
 
-    public static void main(String args[]) {
+    public static ResultSet main(String query) {
+        ResultSet res = null;
         try {
-            // étape 1: charger la classe de driver
             Class.forName("org.postgresql.Driver");
-
-            // étape 2: créer l'objet de connexion
             Connection conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5438/postgres", "postgres",
                     "postgres");
-            // étape 3: créer l'objet statement
-            Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM roles");
-
-            // étape 4: exécuter la requête
-            while (res.next())
-                System.out.println(res.getInt(2) + "  ");
-
-            // étape 5: fermez l'objet de connexion
-            conn.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-
-    public ResultSet query(String query) {
-        ResultSet res = null
-        try {
             Statement stmt = conn.createStatement();
             res = stmt.executeQuery(query);
+            conn.close();
         } catch (Exception e) {
             System.out.println(e);
         }
